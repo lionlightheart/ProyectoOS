f55f90102190d74c64fa7c0a95e35057b5d86be3
Cesar <ceencome01@gmail.com>
2023-10-31 19:02:17 +0100

    Añadimos Logs a copias de seguridad

--- src/clases/Backup.py
+++ src/clases/Backup.py
@@ -20,13 +20,45 @@ class Backup:
         self.save_config()
 
     def execute_backup(self):
+        # Comprobación de carpetas de origen y destino
         if not self.source_folder or not self.destination_folder:
             print("Por favor seleccione primero la carpeta de origen y la carpeta de destino.")
             return
+
+        # Limpiar la pantalla
         os.system("cls")
-        os.system(f"robocopy {self.source_folder} {self.destination_folder}")
+
+        # Realizar la copia de seguridad
+        info_backup = os.popen(f"robocopy {self.source_folder} {self.destination_folder}").read()
         print("Copia de seguridad completada con éxito.")
         input("Presione Enter para continuar...")
+
+        # Verificar si existe el directorio de logs, si no, crearlo
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+
+        # Obtener el contenido de logs existentes
+        contenido_logs = os.listdir('logs')
+        
+        # Filtrar los archivos de log existentes
+        existing_logs = [log for log in contenido_logs if log.startswith('log') and log.endswith('.txt')]
+
+        # Verificar el número más alto entre los logs existentes
+        if existing_logs:
+            # Extraer números de los nombres de archivo de logs existentes
+            existing_logs_numbers = [int(log.split('log')[1].split('.txt')[0]) for log in existing_logs]
+            # Obtener el siguiente número para el nuevo log
+            next_log_number = max(existing_logs_numbers) + 1
+        else:
+            # Si no hay logs existentes, crear el primero
+            next_log_number = 1
+
+        # Nombre del próximo archivo de log
+        next_log_filename = f"log{next_log_number}.txt"
+
+        # Escribir la información de la copia de seguridad en el nuevo archivo de log
+        with open(f"logs/{next_log_filename}", "w") as file:
+            file.write(info_backup)
 
     def save_config(self):
         with open(self.file_path, "w",encoding="utf-8") as f:
